618d0405f88dd712967e7f27de3a85478e288b9c
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-24 14:10:15 +0530

    Create LG logo.py

---
+++ Famous logos in Python/LG logo.py
@@ -0,0 +1,29 @@
+#LG logo in Python turtle
+
+from turtle import *
+color("#990033")
+dot(300)
+up()
+goto(0,120)
+down()
+color("white")
+width(10)
+left(180)
+
+circle(120,270)
+left(90)
+fd(80)
+up()
+goto(-40,50)
+down()
+dot(30)
+
+up()
+goto(0,60)
+down()
+left(90)
+fd(120)
+lt(90)
+fd(40)
+
+done()
