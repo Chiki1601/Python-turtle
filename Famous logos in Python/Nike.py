bfa4946f0712ebf4ab48c64687cd4f01f35c8ad6
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-24 14:10:33 +0530

    Create Nike.py

---
+++ Famous logos in Python/Nike.py
@@ -0,0 +1,18 @@
+#Nike logo in Pytohn turtle
+
+from turtle import *
+color('white')
+bgcolor('black')
+ht()
+lt(180)
+up()
+circle(30,10)
+down()
+begin_fill()
+circle(30,190)
+fd(200)
+lt(173)
+fd(190)
+circle(-18,170)
+end_fill()
+done()
