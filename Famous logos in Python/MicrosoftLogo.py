4a35dc9c6633b28ce6b726328e31b74b95c97e74
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-24 14:37:24 +0530

    Create MicrosoftLogo.py

---
+++ Famous logos in Python/MicrosoftLogo.py
@@ -0,0 +1,50 @@
+import turtle
+
+turtle.bgcolor('Black')
+turtle.colormode(255)
+
+def square():
+    for i in range(4):
+        turtle.forward(200)
+        turtle.right(90)
+    turtle.left(90)
+    
+    
+#yellow box
+turtle.penup()
+turtle.goto(100,-150)
+turtle.pendown()
+turtle.color(255,187,0)
+turtle.begin_fill()
+square()
+turtle.end_fill()
+
+#green box
+turtle.penup()
+turtle.goto(100,-70)
+turtle.pendown()
+turtle.color(124,187,0)
+turtle.begin_fill()
+square()
+turtle.end_fill()
+
+#red box
+turtle.penup()
+turtle.goto(-100,-70)
+turtle.pendown()
+turtle.color(246,83,20)
+turtle.begin_fill()
+square()
+turtle.end_fill()
+
+#blue box
+turtle.penup()
+turtle.goto(-100,-150)
+turtle.pendown()
+turtle.color(0,161,241)
+turtle.begin_fill()
+square()
+turtle.end_fill()
+
+
+turtle.done()
