4eff19219efe4b55450ef89f2a696a269cc9b174
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-24 14:34:19 +0530

    Create Telegram.py

---
+++ Famous logos in Python/Telegram.py
@@ -0,0 +1,45 @@
+#TELEGRAM LOOGO WITH PYTHON
+
+import turtle
+
+won = turtle.Screen()
+won.bgcolor("white")
+won.setup(width=1200,height=700)
+r = turtle.Turtle()
+r.pensize(4)
+r.speed(4)
+
+#for the blue circle
+r.pu()
+r.goto(0,-170)
+r.pd()
+r.pensize(4)
+r.pencolor('royal blue')
+r.begin_fill()
+r.color('royal blue')
+r.circle(180,385)
+r.end_fill()
+
+r.speed(2)
+r.begin_fill()
+r.color('white')
+r.pencolor('white')
+r.lt(108)
+r.pu()
+r.fd(75)
+r.pd()
+r.fd(90)
+r.rt(90)
+r.fd(110)
+r.lt(173)
+r.fd(112)
+r.rt(79)
+r.fd(70)
+r.rt(120)
+r.fd(190)
+r.rt(125)
+r.fd(188)
+r.end_fill()
+
+r.hideturtle()
+turtle.done()
