f2767c7ace153504159c1f5815ffefbc3fc171f6
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-25 12:48:58 +0530

    Create SBILogo.py

---
+++ Famous logos in Python/SBILogo.py
@@ -0,0 +1,27 @@
+
+#SBi logo in Python
+from turtle import *
+
+def main():
+    up()
+    goto(0,-170)
+    color("#2a9df4")
+    begin_fill()
+    down()
+    circle(170)
+    end_fill()
+    lt(90)
+    width(20)
+    color("white")
+    fd(190)
+    lt(90)
+    circle(20)
+    up()
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+ht()
+done()
