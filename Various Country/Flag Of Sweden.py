09f3039f9a7c92e8d49452a1094507348b1b700b
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-28 23:02:51 +0530

    Create Flag Of Sweden.py

---
+++ Various Country/Flag Of Sweden.py
@@ -0,0 +1,34 @@
+#flag of Sweden
+from turtle import *
+
+speed(0)
+setup(800, 500)
+bgcolor("#335cff")
+
+penup()
+goto(-400, -37)
+pendown()
+
+color("yellow")
+begin_fill()
+forward(800)
+left(90)
+forward(75)
+left(90)
+forward(800)
+end_fill()
+
+penup()
+goto(-100, -250)
+pendown()
+
+begin_fill()
+forward(75)
+right(90)
+forward(500)
+right(90)
+forward(75)
+end_fill()
+
+hideturtle()
+done()
