877d84350510dca4c2f6f6451643b0b43757d681
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-28 22:23:08 +0530

    Create Flag Of Austria.py

---
+++ Various Country/Flag Of Austria.py
@@ -0,0 +1,22 @@
+#flag of Austria
+from turtle import *
+
+speed(0)
+setup(800, 500)
+bgcolor("red")
+
+penup()
+goto(-400, -80)
+pendown()
+
+color("white")
+begin_fill()
+forward(800)
+left(90)
+forward(167)
+left(90)
+forward(800)
+end_fill()
+
+hideturtle()
+done()
