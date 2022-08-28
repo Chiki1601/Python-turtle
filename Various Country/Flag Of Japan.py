77dd7ac84a5dbc8edc7c7a81f5e204f548b63dc2
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-28 22:32:21 +0530

    Create Flag Of Japan.py

---
+++ Various Country/Flag Of Japan.py
@@ -0,0 +1,18 @@
+#flag of Japan
+from turtle import *
+
+speed(0)
+setup(800, 500)
+
+# Move to start position
+penup()
+goto(0, -100)
+pendown()
+
+color("red")
+begin_fill()
+circle(120)
+end_fill()
+
+hideturtle()
+done()
