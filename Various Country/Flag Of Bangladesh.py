e1f02b31111cdb5db867a56c6a7bfdb16aee80c4
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-28 22:36:01 +0530

    Create Flag Of Bangladesh.py

---
+++ Various Country/Flag Of Bangladesh.py
@@ -0,0 +1,18 @@
+#flag of Bangla desh in Python 
+from turtle import *
+
+speed(0)
+setup(800, 500)
+bgcolor("seagreen")
+
+penup()
+goto(-100, -100)
+pendown()
+
+color("red")
+begin_fill()
+circle(120)
+end_fill()
+
+hideturtle()
+done()
