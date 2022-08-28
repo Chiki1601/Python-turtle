bc64d423da9288f79fc70d83661e5475619c6ac7
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-28 22:23:33 +0530

    Create Flag Of Ukraine.py

---
+++ Various Country/Flag Of Ukraine.py
@@ -0,0 +1,22 @@
+#flag of Ukraine
+from turtle import *
+
+speed(0)
+setup(800, 500)
+bgcolor("yellow")
+
+penup()
+goto(-400, 250)
+pendown()
+
+color("royalblue")
+begin_fill()
+forward(800)
+right(90)
+forward(250)
+right(90)
+forward(800)
+end_fill()
+
+hideturtle()
+done()
