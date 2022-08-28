43f1e821a287552f9078a18668396884d5e7a4a9
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-28 22:30:58 +0530

    Create Flag Of Hungery.py

---
+++ Various Country/Flag Of Hungery.py
@@ -0,0 +1,36 @@
+#flag of Hungery
+
+from turtle import *
+
+speed(0)
+setup(800, 500)
+
+penup()
+goto(-400, 250)
+pendown()
+
+# Red Rectangle
+color("firebrick")
+begin_fill()
+forward(800)
+right(90)
+forward(167)
+right(90)
+forward(800)
+end_fill()
+
+left(90)
+forward(167)
+
+# Green Rectangle
+color("seagreen")
+begin_fill()
+forward(167)
+left(90)
+forward(800)
+left(90)
+forward(167)
+end_fill()
+
+hideturtle()
+done()
