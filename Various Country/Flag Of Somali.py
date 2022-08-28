fcb2f7d56783e96a7e7d58cf92db41e3af4447dd
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-28 22:35:38 +0530

    Create Flag Of Somali.py

---
+++ Various Country/Flag Of Somali.py
@@ -0,0 +1,16 @@
+# flag of somali
+import turtle 
+
+turtle.speed(1)
+turtle.color("white")
+turtle.bgcolor("cyan")
+turtle.fillcolor("white")
+
+turtle.begin_fill()
+
+for i in range(5):
+    turtle.forward(100)
+    turtle.right(144)
+    
+turtle.end_fill()
+turtle.done()
