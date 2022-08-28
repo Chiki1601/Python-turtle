ef1c5e3b6f69eaa27b74ad35ba6744f3a24a817f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-28 22:33:19 +0530

    Create Flag Of Polish.py

---
+++ Various Country/Flag Of Polish.py
@@ -0,0 +1,21 @@
+#flag of Polish
+from turtle import *
+
+speed(0)
+setup(800, 500)
+
+penup()
+goto(-400, -250)
+pendown()
+
+color("red")
+begin_fill()
+forward(800)
+left(90)
+forward(250)
+left(90)
+forward(800)
+end_fill()
+
+hideturtle()
+done()
