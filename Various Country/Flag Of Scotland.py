681e4e5afcea52c41cb3129ce516daed82b9ab2f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-28 22:40:33 +0530

    Create Flag Of Scotland.py

---
+++ Various Country/Flag Of Scotland.py
@@ -0,0 +1,25 @@
+#Flag of scotland
+from turtle import *
+
+speed(0)
+setup(800, 500)
+bgcolor("royalblue")
+
+penup()
+goto(-400, 250)
+pendown()
+pensize(65)
+
+color("white")
+right(31.85)
+forward(920)
+
+penup()
+goto(-410, -250)
+pendown()
+
+right(296.3)
+forward(920)
+
+hideturtle()
+done()
