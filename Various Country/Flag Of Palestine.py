0a3906f7eceda863f6dfc744848e676ef66b3308
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-28 23:05:20 +0530

    Create Flag Of Palestine.py

---
+++ Various Country/Flag Of Palestine.py
@@ -0,0 +1,51 @@
+#flag Of Palestine
+from turtle import *
+
+speed(0)
+setup(800, 400)
+penup()
+goto(-400, 200)
+pendown()
+
+color("black")
+begin_fill()
+forward(800)
+right(90)
+forward(133)
+right(90)
+forward(800)
+end_fill()
+
+right(180)
+
+color("white")
+begin_fill()
+forward(800)
+right(90)
+forward(133)
+right(90)
+forward(800)
+end_fill()
+
+right(180)
+
+color("#268024")
+begin_fill()
+forward(800)
+right(90)
+forward(133)
+right(90)
+forward(800)
+end_fill()
+
+right(180)
+
+color("#E4312B")
+begin_fill()
+goto(-200, 15)
+left(318)
+forward(-400)
+end_fill()
+
+hideturtle()
+done()
