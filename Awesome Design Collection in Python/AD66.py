109ac21386054772036c608a4841928fca7d9cb9
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-30 15:14:21 +0530

    Create AD66.py

---
+++ Awesome Design Collection in Python/AD66.py
@@ -0,0 +1,31 @@
+#Awesome Design 66
+import turtle as t
+import colorsys
+
+t.hideturtle()
+t.speed(0)
+t.bgcolor("black")
+h = 0
+t.pensize(3)
+
+def Design(ang):
+    t.circle(60+i,90)
+    t.lt(ang)
+    t.circle(60+i,90)
+
+t.pu()
+t.goto(40,0)
+t.pd()
+for i in range(160):
+    c= colorsys.hsv_to_rgb(h,0.8,1)
+    t.pencolor(c)
+    Design(90)
+    Design(90)
+    t.pu()
+    Design(60)
+    Design(60)
+    t.pd()
+    h += 0.01
+t.done()
+
+done()
