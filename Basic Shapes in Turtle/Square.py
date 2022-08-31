6d5d75ed4b3d9bfad87f3e004083f1a80964ad20
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-31 14:55:50 +0530

    Update Square.py

--- Basic Shapes in Turtle/Square.py
+++ Basic Shapes in Turtle/Square.py
@@ -1,3 +1,4 @@
+#method 1
 from turtle import*
 fd(100)  
 rt(90)  
@@ -6,7 +7,19 @@ rt(90)
 fd(100) 
 rt(90)  
 fd(100)  
-
-
 hideturtle()
 done()
+
+
+
+#method 2
+import turtle  
+# Creating turtle  
+t = turtle.Turtle()  
+  
+for i in range(4):  
+    t.fd(100)  
+    t.rt(90)  
+  
+  
+turtle.mainloop()  
