0b66b5545e687c50b86cd6830fedf77a5d9a4c3f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-31 14:53:47 +0530

    Create Circle.py

---
+++ Basic Shapes in Turtle/Circle.py
@@ -0,0 +1,19 @@
+#circle
+
+import turtle  
+# Creating turtle screen  
+t = turtle.Turtle()  
+  
+t.circle(50)  
+  
+turtle.mainloop()  
+
+
+
+#Dot
+# Creating turtle screen  
+t = turtle.Turtle()  
+  
+t.dot(50)  
+  
+turtle.mainloop()  
