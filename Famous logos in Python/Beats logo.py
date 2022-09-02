5759d8dc8bf350280b1e081f3cbb04e2767bee2a
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-09-02 19:15:30 +0530

    Create Beats logo.py

---
+++ Famous logos in Python/Beats logo.py
@@ -0,0 +1,16 @@
+#beats  logo in Python
+from turtle import *
+speed(5)
+bgcolor("black")
+pencolor("#E01F30")
+ht()
+width(60)
+circle(100,572)
+pencolor("black")
+width(33.5)
+seth(90)
+fd(50)
+back(140)
+goto(0,100)
+dot(72,"#E01F30")
+done()
