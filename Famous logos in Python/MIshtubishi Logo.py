7ac889c350ca7608d78ae0a7daa44c44152c46bc
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-09-10 10:44:25 +0530

    Create MIshtubishi Logo.py

---
+++ Famous logos in Python/MIshtubishi Logo.py
@@ -0,0 +1,15 @@
+#Mishtubishi logo in Pytohn
+from turtle import *
+speed(6)
+color("darkred")
+begin_fill()
+for i in range(3):
+    for i in range(2):
+        fd(100)
+        rt(60)
+        fd(100)
+        rt(120)
+    rt(120)
+end_fill()
+ht()
+done()
