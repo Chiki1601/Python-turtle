f9cd3a78e12b287498a69c9f038d9ef35b9720d5
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-02 10:01:56 +0530

    Create Master card logo.py

---
+++ Famous logos in Python/Master card logo.py
@@ -0,0 +1,29 @@
+#mastercard logo in Python
+
+from turtle import *
+speed(8)
+pu()
+goto(-70,-100)
+pd()
+color("#EB001B")
+begin_fill()
+circle(100)
+end_fill()
+
+pu()
+goto(70,-100)
+pd()
+color("#F79E1B")
+begin_fill()
+circle(100)
+end_fill()
+circle(100,225)
+color("#FF5F00")
+begin_fill()
+circle(100,90)
+lt(90)
+circle(100,90)
+end_fill()
+
+ht()
+done()
