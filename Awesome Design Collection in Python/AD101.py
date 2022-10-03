66a6c8aa38a7e5c380ca4740ae47794ae2a04648
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-03 12:00:27 +0530

    Create AD101.py

---
+++ Awesome Design Collection in Python/AD101.py
@@ -0,0 +1,30 @@
+#awesome design 101
+from turtle import *
+import colorsys
+bgcolor("black")
+h = 0.3
+speed(0)
+pensize(4)
+
+def design(ang,len):
+    seth(ang)
+    fd(300-len)
+    seth(ang+60)
+    circle(110-((110/300)*len),90)
+    seth(ang-150)
+    fd(300-len)
+    
+    
+for i in range(12):
+    c = colorsys.hsv_to_rgb(h,0.8,1)
+    pencolor(c)
+    for j in range(30):
+        if j ==29:
+            fillcolor(c)
+            begin_fill()
+            design(i*30,j*5)
+            end_fill()
+    h += 0.15
+    
+ht()
+done()
