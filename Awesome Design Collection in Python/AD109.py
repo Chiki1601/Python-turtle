17147b608788f7e137e6da694741783f724f3191
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-06 10:24:13 +0530

    Create AD109.py

---
+++ Awesome Design Collection in Python/AD109.py
@@ -0,0 +1,28 @@
+#awesome design 109
+from turtle import *
+import colorsys
+bgcolor("black")
+h = 0.99
+speed(0)
+pensize(3)
+
+
+def design(ang,n):
+    circle(60+n,90)
+    lt(ang)
+    circle(60+n,90)
+pu()
+goto(-30,0)
+pd()
+for i in range(150):
+    c = colorsys.hsv_to_rgb(h,0.8,1)
+    pencolor(c)
+    design(151,i)
+    design(59,i)
+    design(151,i)
+    design(59,i)
+    
+    h += 0.001
+
+ht()
+done()
