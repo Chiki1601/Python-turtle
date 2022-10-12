6f65b25a32f77fa32ee947ea131688dc4a48e297
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-12 19:25:06 +0530

    Create Cute wallpaper 9.py

---
+++ Python turtle wallpaper designs/Cute wallpaper 9.py
@@ -0,0 +1,31 @@
+#cute wallpaper 9
+from turtle import *
+import colorsys
+tracer(624)
+bgcolor("black")
+
+def H():
+    h = 0
+    n = 1445
+
+    for i in range(15):
+        c = colorsys.hsv_to_rgb(h,1,1)
+        pencolor(c)
+        up()
+        goto(0,0)
+        down()
+        h +=1/n
+        pensize(7)
+        color("black")
+        fd(i)
+        circle(i,15)
+        for j in range(650):
+            c = colorsys.hsv_to_rgb(h,1,1)
+            h +=1/n
+            pencolor(c)
+            circle(j,11)
+            
+H()           
+            
+ht()
+done()
