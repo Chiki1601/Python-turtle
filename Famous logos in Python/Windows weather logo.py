fe961dc6fcc6bbb5bb1f55251b602b393dc93938
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-19 10:22:50 +0530

    Create Windows weather logo.py

---
+++ Famous logos in Python/Windows weather logo.py
@@ -0,0 +1,28 @@
+#windows weather logo in Pytohn
+
+from turtle import *
+speed(4)
+width(1)
+bgcolor("#eeeeee")
+pu()
+goto(0,-200)
+pd()
+color("#ffa500")
+begin_fill()
+circle(200)
+end_fill()
+circle(200,45)
+color("#f5f5f5")
+begin_fill()
+lt(135)
+fd(100)
+circle(-50,180)
+lt(80)
+circle(-70,130)
+lt(50)
+circle(-66,180)
+fd(40)
+end_fill()
+
+ht()
+done()
